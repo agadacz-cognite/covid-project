eef2afaed5e6c6b72324659d0bae5dcedebbe898
Anna Gadacz <61664776+agadacz-cognite@users.noreply.github.com>
2021-04-22 17:52:12 +0200

    Initial commit

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainWrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default function App(): JSX.Element {
+  return <MainWrapper>(((o(*ﾟ▽ﾟ*)o)))</MainWrapper>;
+}
