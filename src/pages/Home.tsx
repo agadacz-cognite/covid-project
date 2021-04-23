31feed83aee9691d6812d267009b88a7919e9a94
Anna Gadacz <anna.gadacz@cognite.com>
2021-04-23 03:00:17 +0200

    :sparkles: added guidelines summary

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,7 +25,7 @@ export default function Home(): JSX.Element {
   }, []);
 
   return (
-    <Panel>
+    <Panel style={{ maxWidth: '500px' }}>
       <Title level={2}>COVID test rejestration</Title>
       <Title level={4} style={{ margin: 0 }}>
         <a
@@ -35,6 +35,31 @@ export default function Home(): JSX.Element {
           Guidelines
         </a>
       </Title>
+      <ul>
+        <li>
+          It is important that you do not enter Cognite&apos;s office (the 4th
+          and 5th floor) until after you have been tested in Grand Hall. (1st
+          floor).
+        </li>
+        <li>You should not come in earlier than your test appointment.</li>
+        <li>
+          If you have any reason / need to work from the office, please reach
+          out to your people manager, and your manager will reach out to Hanne
+          Natvik and Madeleine.
+        </li>
+        <li>
+          These tests still require employees to continue to take necessary
+          precautions.
+        </li>
+        <li>
+          Employees need to keep a 2 meter distance in the work space - and use
+          every second desk.{' '}
+        </li>
+        <li>
+          External visitors, consultants etc should be avoided / be approved by
+          Hanne Natvik.
+        </li>
+      </ul>
       <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
     </Panel>
   );
