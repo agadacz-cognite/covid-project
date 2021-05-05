4ab2df1bb45de60f8606f1e776c4dd99b6e5f96e
Anna Gadacz <anna.gadacz@cognite.com>
2021-05-05 16:43:12 +0200

    :wrench: setting env config

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,16 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import 'firebase/functions';
 import { errorHandler } from '../shared';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   authDomain: 'covid-project-a32a4.firebaseapp.com',
   databaseURL: 'https://covid-project-a32a4.europe-west1.firebasedatabase.app',
   projectId: 'covid-project-a32a4',
   storageBucket: 'covid-project-a32a4.appspot.com',
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 if (!firebase.apps.length) {
