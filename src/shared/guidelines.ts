1eea2cf63f5d125c276d6c21ad3e4c29fad26c7b
Anna Gadacz <anna.gadacz@cognite.com>
2021-05-06 01:53:41 +0200

    :sparkles: calendar events

---
+++ src/shared/guidelines.ts
@@ -0,0 +1,8 @@
+export const guidelines = [
+  'It is important that you do not enter Cognite office (the 4th and 5th floor) until after you have been tested in Grand Hall. (1st floor)',
+  'You should not come in earlier than your test appointment.',
+  'If you have any reason / need to work from the office, please reach out to your people manager, and your manager will reach out to Hanne Natvik and Madeleine.',
+  'These tests still require employees to continue to take necessary precautions.',
+  'Employees need to keep a 2 meter distance in the work space - and use every second desk.',
+  'External visitors, consultants etc should be avoided / be approved by Hanne Natvik.',
+];
