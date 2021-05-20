d2444f35c2fce6b2a6fcd5b2d4ac213ba85702a7
Anna Gadacz <anna.gadacz@cognite.com>
2021-05-20 11:22:57 +0200

    :lipstick: fixing style for slack link on mobile

--- src/components/SlackLink.tsx
+++ src/components/SlackLink.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Flex } from './Flex';
 
-const Wrapper = styled(Flex)`
+const Wrapper = styled.span`
   background-color: #f0dfdf;
   margin: 8px;
   padding: 8px;
   font-weight: bold;
+  text-align: center;
 `;
 
 export const SlackLink = (): JSX.Element => {
   return (
-    <Wrapper row align justify>
+    <Wrapper>
       All slots are taken and you want to either get one or give away one? Visit
       <a
         href="https://cognitedata.slack.com/archives/C022RUTF21X"
